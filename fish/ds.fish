fa6d85e3fab1bfb932745773178a6cf48d1dd9fb
David Flores <dmousex@gmail.com>
2020-05-26 18:03:38 -0500

    install docker and setup groups

---
+++ fish/ds.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function ds --description 'alias ds=docker-compose ps'
+	docker-compose ps $argv;
+end
---
+++ fish/o.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function o --description 'alias o=xdg-open'
+	xdg-open  $argv;
+end
